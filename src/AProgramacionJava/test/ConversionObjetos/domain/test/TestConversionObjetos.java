a6a4d673aec83de8441723e4db4daa8563432594
Brand-db <jairo.padilla.deb19@gmail.com>
2022-09-13 00:35:43 -0400

    update: conversionObjects

--- src/AProgramacionJava/test/ConversionObjetos/domain/test/TestConversionObjetos.java
+++ src/AProgramacionJava/test/ConversionObjetos/domain/test/TestConversionObjetos.java
@@ -9,20 +9,25 @@ public class TestConversionObjetos {
     public static void main(String[] args) {
 
         Empleado empleado;
+
         empleado = new Empleado("Juan", 500);
         System.out.println("\nempleado = " + empleado);
-
-        empleado = new Escritor("Juan", 500, TipoEscritura.CLASICO);
-        System.out.println("\nempleado = " + empleado);
-        //DOWN CASTING
-        ((Escritor) empleado).getTipoEscritura();
 
         empleado = new Gerente("Juan", 5000, "Sistemas");
         System.out.println("\nempleado = " + empleado);
 
+        empleado = new Escritor("Juan", 500, TipoEscritura.CLASICO);
+        System.out.println("\nempleado = " + empleado);
+
+        //DOWN CASTING cuando el padre se vuelve el hijo
+        System.out.println(empleado.obtenerDetalles());//metodo del padre tiene (autocomplete)
+        ((Escritor) empleado).getTipoEscritura();//el padre usa el metodo del hijo pero se castea
+
+
         //DOWN CASTING
         //conversion de variable tipo padre a hijo(se especifica el tipo con cast)
         Escritor escritor = (Escritor) empleado;
+        escritor.getTipoEscritura();
         //UP CASTING
         //conversion de variable tipo hijo a padre(no se pespeficica el tipo)
         Empleado emplead1 = empleado;
